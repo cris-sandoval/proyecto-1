28e177d48a3497a74af47745ab17c7ff6ee6ba14
unknown <cris.sandoval.cf@gmail.com>
2023-07-16 23:07:50 -0300

    botonoes

---
+++ toppings/cronometro/app.js
@@ -0,0 +1,13 @@
+// Seleccionar los botones.
+const botonInicioPausa = document.querySelector('#boton-inicio-pausa');
+const botonReiniciar = document.querySelector('#boton-reiniciar');
+
+// Variables para almacenar los segundos, minutos y horas.
+let [segundos, minutos, horas] = [0, 0, 0];
+
+// Variables para almacenar el intervalo de tiempo que debe
+// transcurrir para actualizar el cronometro y el estado 
+// del cronometro.
+let intervaloDeTiempo;
+let estadoCronometro = 'pausado'; // Dos estados posibles: 'pausado' o 'andando'.
+
