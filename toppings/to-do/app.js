9aa1a224ced130d39adc373a2fad20de2182065a
unknown <cris.sandoval.cf@gmail.com>
2023-07-24 23:07:58 -0300

    finalizado

--- toppings/to-do/app.js
+++ toppings/to-do/app.js
@@ -22,10 +22,12 @@ function crearTarea() {
         /* icono completar */
         let completar = document.createElement('i')
         completar.classList.add('bi', 'bi-check-circle-fill', 'icono-completar')
+        completar.addEventListener('click',tareaCompletada)
         icon.appendChild(completar)
 
         let eliminar = document.createElement('i')
         eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-elimnar')
+        eliminar.addEventListener('click', eliminarTarea)
         icon.appendChild(eliminar)
 
 
@@ -35,5 +37,20 @@ function crearTarea() {
         alert('Puedes ingresar una tarea')
     }
 }
+function tareaCompletada(e){
+    console.log(e.target);
+    let tarea = e.target.parentNode.parentNode
+    console.log(tarea);
+    tarea.classList.toggle('completada')
+}
+function eliminarTarea(e){
+    let tarea = e.target.parentNode.parentNode
+    tarea.remove()
+}
 
 button.addEventListener('click', crearTarea)
+input.addEventListener('keydown', (e) =>{
+    if(e.key === 'enter'){
+        crearTarea()
+    }
+})
